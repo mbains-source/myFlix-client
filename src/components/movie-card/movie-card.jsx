beb4df72ea414e62241a0b50710a1c3b305378a0
mbains-source <114851491+mbains-source@users.noreply.github.com>
2023-07-16 16:38:49 -0700

    update navbar and movie card and movie view

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,37 +1,84 @@
-import PropTypes from "prop-types";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Button, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './movie-card.scss';
 
-export const MovieCard = ({ movie }) => {
-    return (
-        <Card className="h-100">
-            <Card.Img variant="top" src={movie.image} />
-            <Card.Body>
-                <Card.Title>{movie.title}</Card.Title>
-                <Card.Text>Director: {movie.director.name}</Card.Text>
-                <Card.Text>Description: {movie.description}</Card.Text>
-                <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
-                    <Button variant="link">
-                        Open
-                    </Button>
-                </Link>
-            </Card.Body>
-        </Card>
-    );
+export const MovieCard = ({ fav, movie, onAddToFavorites, onRemoveFromFavorites }) => {
+  const maxDescriptionLength = 100;
+  const truncatedDescription =
+    movie.description.length > maxDescriptionLength
+      ? `${movie.description.substring(0, maxDescriptionLength)}...`
+      : movie.description;
+
+  const [isFavorite, setIsFavorite] = useState(fav);
+
+      useEffect(() => {
+        setIsFavorite(fav);
+      }, [fav]);
+
+  const handleAddToFavorites = (event) => {
+    event.preventDefault();
+    onAddToFavorites(movie.id);
+    setIsFavorite(true);
+  };
+
+  const handleRemoveFromFavorites = (event) => {
+    event.preventDefault();
+    onRemoveFromFavorites(movie.id);
+    setIsFavorite(false);
+  };
+
+  const handleFavoriteClick = (event) => {
+    event.preventDefault();
+    if (isFavorite) {
+      setIsFavorite(false);
+    } else {
+      handleAddToFavorites(event);
+    }
+  };
+  
+  return (
+    <Card className="h-100">
+      <Card.Img
+        className="movie-card-img"
+        variant="top"
+        src={movie.image}
+        alt={movie.title}
+      />
+      <Card.Body>
+        <Card.Title>{movie.title}</Card.Title>
+        <Card.Text>{truncatedDescription}</Card.Text>
+        <div className="button-group">
+          <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
+            <Button variant="link" aria-label="Open movie details">Open</Button>
+          </Link>
+          <Button
+            className="favorite-button"
+            variant={isFavorite ? 'primary' : 'primary'}
+            onClick={!isFavorite ? handleFavoriteClick : handleRemoveFromFavorites} //use handleFavoriteClick for favorite button click
+          >
+            {isFavorite ? 'Remove from Favorites' : 'Add to Favorites'}
+          </Button>
+        </div>
+      </Card.Body>
+    </Card>
+  );
 };
 
 MovieCard.propTypes = {
-    movie: PropTypes.shape({
-        title: PropTypes.string,
-        description: PropTypes.string,
-        director: PropTypes.shape({
-            name: PropTypes.string,
-            bio: PropTypes.string
-        }),
-        genre: PropTypes.shape({
-            name: PropTypes.string,
-            description: PropTypes.string
-        }),
-    }).isRequired
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    genre: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired
+    }).isRequired,
+    director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+  onAddToFavorites: PropTypes.func.isRequired,
 };
--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,46 +1,55 @@
-import Button from "react-bootstrap";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import "./navigation-bar.scss";
 
 export const NavigationBar = ({ user, onLoggedOut }) => {
-    return (
-        <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
-            <Container>
-                <Navbar.Brand as={Link} to="/" >
-                    MyFlixApp
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navabar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        {!user && (
-                            <>
-                            <Nav.Link as={Link} to="/login">
-                                Login
-                            </Nav.Link>
-                            <Nav.Link as={Link} to="/signup">
-                                Signup
-                            </Nav.Link>
-                            </>
-                        )}
+  const [loggedIn, setLoggedIn] = useState(false);
 
-                        {user && (
-                            <>
-                            <Nav.Link as={Link} to="/">
-                                Home
-                            </Nav.Link>
-                            <Nav.Link onClick={onLoggedOut}>
-                                Logout
-                            </Nav.Link>
-                            <Nav.Link as={Link} to="/profile">
-                                My Profile
-                            </Nav.Link>
-                            </>
-                        )}
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    )
-}
+  useEffect(() => {
+    if (user) {
+      setLoggedIn(true);
+    } else {
+      setLoggedIn(false);
+    }
+  }, [user]);
+
+  return (
+    <Navbar className="custom-navbar" expand="lg">
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          Movies App
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            {loggedIn && (
+              <Nav.Link as={Link} to={`/users/${user?.UserName}`}>
+                Profile
+              </Nav.Link>
+            )}
+            {!loggedIn && (
+              <>
+                <Nav.Link as={Link} to="/signup">
+                  Sign up
+                </Nav.Link>
+                <Nav.Link as={Link} to="/login">
+                  Log in
+                </Nav.Link>
+              </>
+            )}
+          </Nav>
+          {loggedIn && (
+            <Button variant="outline-secondary" onClick={onLoggedOut}>
+              Log out
+            </Button>
+          )}
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
