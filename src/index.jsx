ea4e46eec56c4ce00bb4e37a3fb5987586b5f98d
mbains-source <114851491+mbains-source@users.noreply.github.com>
2023-07-16 16:25:51 -0700

    update index files

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,16 @@
-import { createRoot } from "react-dom/client";
+import { createRoot } from 'react-dom/client';
 import { MainView } from "./components/main-view/main-view";
-import "./index.scss";
-import Container from "react-bootstrap/Container";
-
+import Container from 'react-bootstrap/Container';
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
 
 // Main component (will eventually use all the others)
 const MyFlixApplication = () => {
-    return (
-        <Container>
-            <MainView />
-        </Container>
-    );
+  return (
+    <Container className="my-flix">
+        <MainView />
+    </Container>
+  );
 };
 
 // Finds the root of your app
