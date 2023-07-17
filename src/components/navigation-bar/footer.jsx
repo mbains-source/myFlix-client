2f6306479415be310c79b4844673898ad68ae46a
mbains-source <114851491+mbains-source@users.noreply.github.com>
2023-07-16 17:11:31 -0700

    update footer

--- src/components/navigation-bar/footer.jsx
+++ src/components/navigation-bar/footer.jsx
@@ -6,7 +6,7 @@ export const Footer = () => {
     return (
         <footer className="mt-auto py-3">
           <div className="container">
-            <p className="text-center my-footer-text">&copy; 2023 TamarFlix</p>
+            <p className="text-center my-footer-text">&copy; 2023 MyFlix</p>
           </div>
         </footer>
       );
